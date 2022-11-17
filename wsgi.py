8f8205f28d16d8a48080f9d63585328a1a73224b
CHunYenc <yuhnoesty0402@hotmail.com.tw>
2022-11-17 22:06:12 +0800

    fix: update to production.

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,6 @@ import eventlet
 from eventlet import wsgi
 from app import create_app
 
-app = create_app("development")
-# app = create_app("production")
+# app = create_app("development")
+app = create_app("production")
 wsgi.server(eventlet.listen(("", 8080)), app)
