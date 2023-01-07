ad9d1e17f0e281b8b59fd910a5b7f18c5401c2af
CHunYenc <yuhnoesty0402@hotmail.com.tw>
2023-01-07 14:58:10 +0800

    fix: delete comma in variables.

--- core/variables.py
+++ core/variables.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env.
 
 REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
-DB_NAME = os.getenv("DB_NAME", "default"),
-DB_USER = os.getenv("DB_USER", "default"),
-DB_PASSWORD = os.getenv("DB_PASSWORD", "default"),
-DB_HOST = os.getenv("DB_HOST", "default"),
+DB_NAME = os.getenv("DB_NAME", "default")
+DB_USER = os.getenv("DB_USER", "default")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "default")
+DB_HOST = os.getenv("DB_HOST", "default")
 DB_PORT = int(os.getenv("DB_PORT", "default"))
