bc18179471dcdac6ad45621fc52511a1c34cb98b
CHunYenc <yuhnoesty0402@hotmail.com.tw>
2023-01-08 22:08:24 +0800

    fix: cryptocom percentage.

--- apps/pricing/views.py
+++ apps/pricing/views.py
@@ -1,4 +1,6 @@
+from django import template
 import json
+import logging
 from django.shortcuts import render
 from django.http.response import Http404
 from django.core.cache import cache
@@ -29,6 +31,7 @@ def get_pricing(request, exchange, symbol_a, symbol_b):
     symbol_B = str.upper(symbol_b)
     r_data = cache.get(f"{c_exchange}")
     if r_data is None:
+        logging.info(f"The {c_exchange} data not exist.")
         raise Http404()
     result = json.loads(r_data)
     symbol_data = result.get(
@@ -37,5 +40,10 @@ def get_pricing(request, exchange, symbol_a, symbol_b):
     exist = True if type(symbol_data) is dict else False
     # system not symbol data
     if not exist:
+        logging.info(
+            f"The {c_exchange}'s {symbol_A}/{symbol_B} data not exist.")
         raise Http404()
+    # 如果沒有漲跌幅度時，提供一個漲跌幅度
+    if symbol_data['percentage'] is None:
+        symbol_data['percentage'] = float(symbol_data['info']['c']) * 100
     return render(request, 'price.html', context={"data": symbol_data, "exchange": exchange.upper()})
